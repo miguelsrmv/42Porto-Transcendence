aafae6a8fe3bc3d45cf1f4d6b5c112c28136fee3
damachad <davidmachado98@hotmail.com>
2025-03-04 15:35:59 +0000

    Change to API to TS

---
+++ srcs/requirements/backend/src/index.ts
@@ -0,0 +1,17 @@
+import fastify from 'fastify'
+
+const server = fastify({
+	logger: true
+})
+
+server.get('/', async (request, reply) => {
+	reply.send({ hello: 'world' })
+})
+
+server.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
