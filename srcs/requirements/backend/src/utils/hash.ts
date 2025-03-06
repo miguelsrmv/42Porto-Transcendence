ddc587a3ec02b9e79cc69e6516aa7850984d34c9
damachad <davidmachado98@hotmail.com>
2025-03-06 15:02:29 +0000

    Formatting update

--- srcs/requirements/backend/src/utils/hash.ts
+++ srcs/requirements/backend/src/utils/hash.ts
@@ -1,17 +1,27 @@
-import crypto from 'crypto'
+import crypto from "crypto";
 
 export function hashPassword(password: string) {
-	const salt = crypto.randomBytes(16).toString('hex')
+  const salt = crypto.randomBytes(16).toString("hex");
 
-	const hash = crypto.pbkdf2Sync(password, salt, 100, 64, 'sha512').toString('hex')
+  const hash = crypto
+    .pbkdf2Sync(password, salt, 100, 64, "sha512")
+    .toString("hex");
 
-	return ({ hash, salt })
+  return { hash, salt };
 }
 
-export function verifyPassword({candidatePassword, hash, salt}: 
-	{candidatePassword: string, hash: string, salt: string}) {
-	
-	const candidateHash = crypto.pbkdf2Sync(candidatePassword, salt, 100, 64, 'sha512').toString('hex')
+export function verifyPassword({
+  candidatePassword,
+  hash,
+  salt,
+}: {
+  candidatePassword: string;
+  hash: string;
+  salt: string;
+}) {
+  const candidateHash = crypto
+    .pbkdf2Sync(candidatePassword, salt, 100, 64, "sha512")
+    .toString("hex");
 
-	return candidateHash === hash
+  return candidateHash === hash;
 }
