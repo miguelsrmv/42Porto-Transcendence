5ea8c68096bf4ae43205973f9a25c649ceb55150
damachad <davidmachado98@hotmail.com>
2025-03-07 15:29:03 +0000

    Add number_of_users macro

--- srcs/requirements/backend/scripts/populate_db.ts
+++ srcs/requirements/backend/scripts/populate_db.ts
@@ -2,9 +2,11 @@
 import { prisma } from "../src/utils/prisma";
 import { faker } from "@faker-js/faker";
 
+const NUMBER_OF_USERS = 4;
+
 async function seedUsers() {
   await prisma.user.deleteMany();
-  for (let index = 0; index < 4; index++) {
+  for (let index = 0; index < NUMBER_OF_USERS; index++) {
     await prisma.user.create({
       data: {
         name: faker.person.fullName(),
