b1eb521838de99c30e7f1d72a0c233ca07306cc2
damachad <davidmachado98@hotmail.com>
2025-03-07 12:44:15 +0000

    feat: add createUser

--- srcs/requirements/backend/src/controllers/user.controller.ts
+++ srcs/requirements/backend/src/controllers/user.controller.ts
@@ -1,5 +1,16 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { prisma } from "../utils/prisma";
+import { hashPassword } from "../utils/hash";
+
+interface IParams {
+  id: string;
+}
+
+interface UserCreate {
+  name: string;
+  email: string;
+  password: string;
+}
 
 export async function getAllUsers(
   request: FastifyRequest,
@@ -13,10 +24,6 @@ export async function getAllUsers(
   }
 }
 
-interface IParams {
-  id: string;
-}
-
 export async function getUserById(
   request: FastifyRequest<{ Params: IParams }>,
   reply: FastifyReply
@@ -24,6 +31,26 @@ export async function getUserById(
   try {
     const user = await prisma.user.findUniqueOrThrow({
       where: { id: request.params.id },
+    });
+    reply.send(user);
+  } catch (error) {
+    reply.status(500).send(error);
+  }
+}
+
+export async function createUser(
+  request: FastifyRequest<{ Body: UserCreate }>,
+  reply: FastifyReply
+) {
+  try {
+    const { hash, salt } = hashPassword(request.body.email);
+    const user = await prisma.user.create({
+      data: {
+        name: request.body.name,
+        email: request.body.email,
+        hashedPassword: hash,
+        salt: salt,
+      },
     });
     reply.send(user);
   } catch (error) {
--- srcs/requirements/backend/src/routes/user.routes.ts
+++ srcs/requirements/backend/src/routes/user.routes.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from "fastify";
-import { getAllUsers, getUserById } from "../controllers/user.controller";
+import { getAllUsers, getUserById, createUser } from "../controllers/user.controller";
 
 export async function userRoutes(fastify: FastifyInstance) {
   fastify.get("/", getAllUsers);
   fastify.get("/:id", getUserById);
+  fastify.post("/create", createUser);
 }
