bbe3a9fe555b074b4d04f8ba6470f5fc836c7f40
damachad <davidmachado98@hotmail.com>
2025-03-11 18:08:54 +0000

    Add comments

--- srcs/requirements/backend/init.sh
+++ srcs/requirements/backend/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npx prisma migrate dev
-npm run build
-chmod -R 755 ./dist
-exec npm start
+npx prisma migrate dev     # Apply migrations to the database and generate prisma client
+npm run build              # Generate the .js files in dist/
+chmod -R 755 ./dist        # Give execution permission to dist/
+exec npm start             # Execute dist/app.js
