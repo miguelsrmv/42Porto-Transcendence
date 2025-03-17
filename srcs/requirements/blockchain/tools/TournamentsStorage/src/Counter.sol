4505fbfcb9b6789273560a30eb738406e25ef26d
Tiago Martins <tiago86martins@gmail.com>
2025-03-17 21:39:06 +0000

    added basic contract for 4 players tournament

--- srcs/requirements/blockchain/tools/TournamentsStorage/src/Counter.sol
+++ srcs/requirements/blockchain/tools/TournamentsStorage/src/Counter.sol
@@ -1,14 +1,54 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
 
-contract Counter {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract TournamentsStorage {
+    struct Tournament {
+        uint256 id;
+        string name;
+        uint256 date;
+        uint256 time;
+        uint256 maxParticipants;
+        uint256[] participants;
     }
 
-    function increment() public {
-        number++;
+    Tournament[] public tournaments;
+
+    uint8 constant MAX_PARTICIPANTS = 4;
+
+    function createTournament(
+        string memory _name,
+        uint256 _date,
+        uint256 _time
+    ) public {
+        tournaments.push(
+            Tournament({
+                id: tournaments.length,
+                name: _name,
+                date: _date,
+                time: _time,
+                maxParticipants: MAX_PARTICIPANTS,
+                participants: new uint256[](0)
+            })
+        );
+    }
+
+    function getTournaments() public view returns (Tournament[] memory) {
+        return tournaments;
+    }
+
+    function getTournament(
+        uint256 _id
+    ) public view returns (Tournament memory) {
+        return tournaments[_id];
+    }
+
+    function joinTournament(uint256 _id) public {
+        tournaments[_id].participants.push(1);
+    }
+
+    function getParticipants(
+        uint256 _id
+    ) public view returns (uint256[] memory) {
+        return tournaments[_id].participants;
     }
 }
