ab9833c7046a96240c50ff81c9cab2179761624b
damachad <davidmachado98@hotmail.com>
2025-03-18 17:45:54 +0000

    Add Vitest testing

---
+++ srcs/requirements/backend/tests/api_routes.test.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "vitest";
+import app from "../src/app";
+
+test("GET / should return 'greetings Welcome to the ft_transcendence API'", async () => {
+  const response = await app.inject({
+    method: "GET",
+    url: "/",
+  });
+
+  expect(response.statusCode).toBe(200);
+  expect(response.json()).toEqual({
+    greetings: "Welcome to the ft_transcendence API",
+  });
+});
