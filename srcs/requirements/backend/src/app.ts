f77e254caf42cab9c12278464c5ab1c7c26abb35
damachad <davidmachado98@hotmail.com>
2025-03-18 17:45:40 +0000

    Separate server setting up logic

--- srcs/requirements/backend/src/app.ts
+++ srcs/requirements/backend/src/app.ts
@@ -2,22 +2,16 @@ import fastify from "fastify";
 import { userRoutes } from "./routes/user.routes";
 import jwtPlugin from "./middlewares/auth";
 
-const server = fastify({
+const app = fastify({
   logger: true,
 });
 
-server.register(jwtPlugin);
+app.register(jwtPlugin);
 
-server.get("/", async (request, reply) => {
+app.get("/", async (request, reply) => {
   reply.send({ greetings: "Welcome to the ft_transcendence API" });
 });
 
-server.register(userRoutes, { prefix: "/users" });
+app.register(userRoutes, { prefix: "/users" });
 
-server.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-});
+export default app;
