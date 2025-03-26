3e9828cdce1733e7a8ee204df7c2a5d30f4c7ea5
damachad <davidmachado98@hotmail.com>
2025-03-26 20:15:30 +0000

    Enhance user routes with validation schemas for CRUD operations

--- srcs/requirements/backend/src/routes/user.routes.ts
+++ srcs/requirements/backend/src/routes/user.routes.ts
@@ -7,14 +7,20 @@ import {
   deleteUser,
   login,
 } from '../controllers/user.controller';
-import { createUserSchema } from '../schemas/user.schema';
+import {
+  createUserSchema,
+  deleteUserSchema,
+  getUserByIdSchema,
+  loginSchema,
+  updateUserSchema,
+} from '../schemas/user.schema';
 
 // NOTE: Insert '{ onRequest: [fastify.jwtAuth] }' before handler to protect route
 export async function userRoutes(fastify: FastifyInstance) {
   fastify.get('/', getAllUsers);
-  fastify.get('/:id', getUserById);
+  fastify.get('/:id', { schema: getUserByIdSchema }, getUserById);
   fastify.post('/create', { schema: createUserSchema }, createUser);
-  fastify.put('/:id', updateUser);
-  fastify.delete('/:id', deleteUser);
-  fastify.post('/login', login);
+  fastify.put('/:id', { schema: updateUserSchema }, updateUser);
+  fastify.delete('/:id', { schema: deleteUserSchema }, deleteUser);
+  fastify.post('/login', { schema: loginSchema }, login);
 }
