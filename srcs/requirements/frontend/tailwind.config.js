d8cc429a48921f22ae748594b35f1e59b03b408e
Miguel Sá <miguel.srmv@gmail.com>
2025-03-29 12:03:39 +0000

    Super Smash Bros CSS

--- srcs/requirements/frontend/tailwind.config.js
+++ srcs/requirements/frontend/tailwind.config.js
@@ -13,9 +13,22 @@ module.exports = {
         'lg': '2px 2px 4px rgba(0, 0, 0, 0.7)',
         'xl': '4px 4px 8px rgba(0, 0, 0, 0.6)',
       },
+      clipPath: {
+      'polygon': 'polygon(0 30%, 100% 00%, 100% 100%, 0% 100%)',
+
       }
     },
   },
   plugins: [
-    require('tailwindcss-textshadow')
+    require('tailwindcss-textshadow'),
+    require('@tailwindcss/forms'),
+    // Add a custom plugin for clip-path
+    function({ addUtilities, theme }) {
+      const newUtilities = {}
+      Object.entries(theme('clipPath') || {}).forEach(([name, value]) => {
+        newUtilities[`.clip-${name}`] = { clipPath: value }
+      })
+      addUtilities(newUtilities)
+    }
+  ],
 }
