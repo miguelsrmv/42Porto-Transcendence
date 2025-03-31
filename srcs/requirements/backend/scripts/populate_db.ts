151ba88a2b095a7ac3a486b63bb5a2291a4724ba
damachad <davidmachado98@hotmail.com>
2025-03-31 14:53:49 +0100

    Update database seeding script to establish friendships

--- srcs/requirements/backend/scripts/populate_db.ts
+++ srcs/requirements/backend/scripts/populate_db.ts
@@ -1,4 +1,5 @@
 /* Script to interact with the database */
+import { Profile } from '@prisma/client';
 import { prisma } from '../src/utils/prisma';
 import { faker } from '@faker-js/faker';
 
@@ -7,11 +8,26 @@ const NUMBER_OF_USERS = 4;
 async function seedUsers() {
   await prisma.user.deleteMany();
   for (let index = 0; index < NUMBER_OF_USERS; index++) {
+    const username = faker.internet.username().replace(/\./g, '_');
+    const email = `${username.toLowerCase()}@example.com`;
     await prisma.user.create({
       data: {
-        username: faker.internet.username(),
-        email: faker.internet.email(),
+        username: username,
+        email: email,
         hashedPassword: faker.internet.password(),
+      },
+    });
+  }
+}
+
+async function createFriends(profiles: Profile[]) {
+  for (let index = 0; index < profiles.length; index++) {
+    const profile = profiles[index];
+    const friendId = profiles[(index + 1) % profiles.length].id;
+    await prisma.friend.create({
+      data: {
+        profileId: profile.id,
+        friendId: friendId,
       },
     });
   }
@@ -20,6 +36,8 @@ async function seedUsers() {
 async function main() {
   try {
     await seedUsers();
+    const profiles = await prisma.profile.findMany();
+    await createFriends(profiles);
     console.log(await prisma.user.findMany({ include: { profile: true } }));
   } catch (e) {
     console.error(e);
