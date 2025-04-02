3c00c96645b5acaf4e422d6cbe1474c168f56527
damachad <davidmachado98@hotmail.com>
2025-04-02 17:18:21 +0100

    Add match settings and update match model: create MatchSettings table, redefine Match structure, and implement match controller and schema

---
+++ srcs/requirements/backend/src/controllers/match.controller.ts
@@ -0,0 +1,65 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { prisma } from '../utils/prisma';
+import { handleError } from '../utils/errorHandler';
+import { MatchMode, MatchSettings } from '@prisma/client';
+
+interface MatchCreate {
+  mode: MatchMode;
+  player1Id: string;
+  player2Id: string;
+  settings: MatchSettings;
+}
+
+export async function getPlayerMatches(
+  request: FastifyRequest<{ Params: IParams }>,
+  reply: FastifyReply,
+) {
+  try {
+    const matches = await prisma.match.findMany({
+      where: {
+        OR: [{ player1Id: request.params.id }, { player2Id: request.params.id }],
+      },
+    });
+    reply.send(matches);
+  } catch (error) {
+    handleError(error, reply);
+  }
+}
+
+export async function getMatchById(
+  request: FastifyRequest<{ Params: IParams }>,
+  reply: FastifyReply,
+) {
+  try {
+    const match = await prisma.match.findUniqueOrThrow({
+      where: { id: request.params.id },
+    });
+    reply.send(match);
+  } catch (error) {
+    handleError(error, reply);
+  }
+}
+
+export async function createMatch(
+  request: FastifyRequest<{ Body: MatchCreate }>,
+  reply: FastifyReply,
+) {
+  try {
+    const { player1Id, player2Id, mode } = request.body;
+    const settings = request.body.settings as MatchSettings;
+
+    const match = await prisma.match.create({
+      data: {
+        mode: mode,
+        player1Id: player1Id,
+        player2Id: player2Id,
+        settings: {
+          create: settings,
+        },
+      },
+    });
+    reply.send(match);
+  } catch (error) {
+    handleError(error, reply);
+  }
+}
