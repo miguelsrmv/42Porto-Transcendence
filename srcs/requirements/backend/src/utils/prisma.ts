53d5a432e3b1214a9c42028614444de8c6cbd8c5
damachad <davidmachado98@hotmail.com>
2025-04-04 15:45:41 +0100

    Refactor Prisma client setup: move user creation logic to a dedicated extension for better organization and maintainability

--- srcs/requirements/backend/src/utils/prisma.ts
+++ srcs/requirements/backend/src/utils/prisma.ts
@@ -1,22 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import { hashPassword } from './hash';
+import { userExtension } from './prismaExtensions';
 
-export const prisma = new PrismaClient().$extends({
-  query: {
-    user: {
-      async create({ args, query }) {
-        const { hash, salt } = hashPassword(args.data.hashedPassword);
-        args.data.hashedPassword = hash;
-        args.data.salt = salt;
-        args.data.player = {
-          create: {
-            name: args.data.username,
-            bio: `Hello, I am ${args.data.username}!`,
-            avatarUrl: 'static/avatar/default/1.png',
-          },
-        };
-        return query(args);
-      },
-    },
-  },
-});
+export const prisma = new PrismaClient().$extends(userExtension);
