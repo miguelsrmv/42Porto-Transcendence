57f6ae178724aa5d9e29ca072411b744b23d0dc6
damachad <davidmachado98@hotmail.com>
2025-04-04 10:33:48 +0100

    Add tournament controller, routes, and schemas for tournament management

---
+++ srcs/requirements/backend/src/controllers/tournament.controller.ts
@@ -0,0 +1,97 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { prisma } from '../utils/prisma';
+import { handleError } from '../utils/errorHandler';
+import { TournamentStatus } from '@prisma/client';
+
+interface TournamentCreate {
+  name?: string;
+  maxParticipants: number;
+  createdBy: string;
+}
+
+interface TournamentUpdate {
+  status: TournamentStatus;
+  currentRound: number;
+}
+
+export async function getPlayerTournaments(
+  request: FastifyRequest<{ Params: { id: string } }>,
+  reply: FastifyReply,
+) {
+  try {
+    const tournaments = await prisma.tournament.findMany({
+      include: {
+        participants: true,
+      },
+      where: {
+        participants: {
+          some: {
+            playerId: request.params.id,
+          },
+        },
+      },
+    });
+    reply.send(tournaments);
+  } catch (error) {
+    handleError(error, reply);
+  }
+}
+
+export async function getTournamentById(
+  request: FastifyRequest<{ Params: { id: string } }>,
+  reply: FastifyReply,
+) {
+  try {
+    const tournament = await prisma.tournament.findUniqueOrThrow({
+      where: { id: request.params.id },
+      include: {
+        participants: true,
+      },
+    });
+    reply.send(tournament);
+  } catch (error) {
+    handleError(error, reply);
+  }
+}
+
+export async function createTournament(
+  request: FastifyRequest<{ Body: TournamentCreate }>,
+  reply: FastifyReply,
+) {
+  try {
+    const { name, maxParticipants, createdBy } = request.body;
+
+    const tournament = await prisma.tournament.create({
+      data: {
+        name: name,
+        maxParticipants: maxParticipants,
+        createdBy: {
+          connect: { id: createdBy },
+        },
+      },
+    });
+    reply.send(tournament);
+  } catch (error) {
+    handleError(error, reply);
+  }
+}
+
+export async function updateTournament(
+  request: FastifyRequest<{ Params: { id: string }; Body: TournamentUpdate }>,
+  reply: FastifyReply,
+) {
+  try {
+    const { status, currentRound } = request.body;
+
+    const tournament = await prisma.tournament.update({
+      where: { id: request.params.id },
+      data: {
+        status: status,
+        currentRound: currentRound,
+      },
+    });
+    reply.send(tournament);
+  } catch (error) {
+    handleError(error, reply);
+  }
+}
--- srcs/requirements/backend/src/routes/match.routes.ts
+++ srcs/requirements/backend/src/routes/match.routes.ts
@@ -9,7 +9,7 @@ import { createMatchSchema, updateMatchSchema } from '../schemas/match.schema';
 import { getByIdSchema } from '../schemas/global.schema';
 
 // NOTE: Insert '{ onRequest: [fastify.jwtAuth] }' before handler to protect route
-export async function playerRoutes(fastify: FastifyInstance) {
+export async function matchRoutes(fastify: FastifyInstance) {
   fastify.get('/', getPlayerMatches);
   fastify.post('/', { schema: createMatchSchema }, createMatch);
   fastify.get('/:id', { schema: getByIdSchema }, getMatchById);
