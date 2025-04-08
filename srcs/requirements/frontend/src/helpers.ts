3ef3dd1e5cc2801581dda8897ad2a817cedf29cc
Miguel Sá <miguel.srmv@gmail.com>
2025-04-08 10:31:10 +0100

    Simplified Header / Main loading

--- srcs/requirements/frontend/src/helpers.ts
+++ srcs/requirements/frontend/src/helpers.ts
@@ -2,11 +2,3 @@ export function wait(seconds: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, seconds * 1000));
 }
 
-export function removeHeightClasses(element: HTMLElement | null) {
-    if (!element) return;
-    element.classList.forEach(cls => {
-        if (/^h-\[\d+%?\]$/.test(cls) || cls === "h-[full]" || cls === "h-full") {
-            element.classList.remove(cls);
-        }
-    });
-}
