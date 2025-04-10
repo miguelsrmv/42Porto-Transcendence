dddc90a59f0dfed520e31c25473f2eea48daad75
tiaferna <tiaferna@student.42porto.com>
2025-04-10 15:36:39 +0100

    added a console log

--- srcs/requirements/blockchain/tools/contracts/TournamentsStorage/script/DeployTournamentsStorage.s.sol
+++ srcs/requirements/blockchain/tools/contracts/TournamentsStorage/script/DeployTournamentsStorage.s.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {TournamentsStorage} from "../src/TournamentsStorage.sol";
 
 contract DeployTournamentsStorage is Script {
     function run() external returns (TournamentsStorage) {
         vm.startBroadcast();
         TournamentsStorage tournamentsStorage = new TournamentsStorage();
+        console.log("Created a new TournamentsStorage contract!");
         vm.stopBroadcast();
         return tournamentsStorage;
     }
