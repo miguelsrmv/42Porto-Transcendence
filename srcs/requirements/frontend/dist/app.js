e3d4177ddcdb3d98dfc29178875d09f04b8c0e71
Miguel Sá <miguel.srmv@gmail.com>
2025-04-13 09:05:16 +0100

    Small docker container fix

---
+++ srcs/requirements/frontend/dist/app.js
@@ -0,0 +1,23 @@
+/**
+ * @file app.ts
+ * @brief Loads other scripts and sets up scripts
+ *
+ * This file contains the logic to handle navigation events and load the appropriate
+ * views into the main application container. It listens for the DOMContentLoaded event
+ * to load the default view and custom navigation events to switch views.
+ */
+import { initializeRouter } from './core/router.js';
+export let loginStatus = "";
+// #TODO: Remove loginStatus and replace it by checking JWT token
+export function setLoginStatus(status) {
+    loginStatus = status;
+}
+/**
+ * @brief Event listener for DOMContentLoaded.
+ *
+ * Initializes routing when the DOM content is fully loaded and parsed.
+ */
+document.addEventListener("DOMContentLoaded", () => {
+    initializeRouter();
+});
+//# sourceMappingURL=app.js.map
