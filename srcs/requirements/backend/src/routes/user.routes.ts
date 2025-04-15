ee6eae5f1cd65c1b901a70ec9ef9d015763d6ff0
damachad <davidmachado98@hotmail.com>
2025-04-15 10:40:03 +0100

    Add preValidation hook to createUser

--- srcs/requirements/backend/src/routes/user.routes.ts
+++ srcs/requirements/backend/src/routes/user.routes.ts
@@ -9,12 +9,13 @@ import {
 } from '../controllers/user.controller';
 import { createUserSchema, loginSchema, updateUserSchema } from '../schemas/user.schema';
 import { getByIdSchema } from '../schemas/global.schema';
+import { userCreateValidation } from '../validation/users.validation';
 
 // NOTE: Insert '{ onRequest: [fastify.jwtAuth] }' before handler to protect route
 export async function userRoutes(fastify: FastifyInstance) {
   fastify.get('/', { onRequest: [fastify.jwtAuth] }, getAllUsers);
   fastify.get('/:id', { schema: getByIdSchema }, getUserById);
-  fastify.post('/', { schema: createUserSchema }, createUser);
+  fastify.post('/', { schema: createUserSchema, preValidation: userCreateValidation }, createUser);
   fastify.patch('/:id', { schema: updateUserSchema }, updateUser);
   fastify.delete('/:id', { schema: getByIdSchema }, deleteUser);
   fastify.post('/login', { schema: loginSchema }, login);
