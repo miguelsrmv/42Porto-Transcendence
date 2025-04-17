893c179766414f1b5b61d6d3c09366c8aaa38ba7
Miguel Sá <miguel.srmv@gmail.com>
2025-04-17 11:12:53 +0100

    Organized folder logic

---
+++ srcs/requirements/frontend/dist/features/game/gameApp/game.js
@@ -0,0 +1,2 @@
+export function initializeGame() { }
+//# sourceMappingURL=game.js.map
--- srcs/requirements/frontend/src/features/game/gamePage.ts
+++ srcs/requirements/frontend/src/features/game/gamePage.ts
@@ -1,9 +1,13 @@
+import { initializeGame } from "./gameApp/game.js"
+
 /**
- * @brief Adds event listeners for the landing view.
+ * @brief Adds event listeners for the game page.
  * 
  * This function sets up the event listener for the landing button, which navigates to the home view upon click.
+ * TODO: Edit this Doxygen comment
  */
 export function initializeView(): void {
+	initializeGame();
 }
 
 
