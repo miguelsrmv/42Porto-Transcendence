09a2bf409f0c3876c59b874ad85faee47be0e511
Miguel Sá <miguel.srmv@gmail.com>
2025-04-17 10:23:53 +0100

    Some UI optimization

--- srcs/requirements/frontend/dist/features/localPlay/localPlay.js
+++ srcs/requirements/frontend/dist/features/localPlay/localPlay.js
@@ -11,42 +11,40 @@ import { getGameType, createBackgroundLoop, createCharacterLoop } from "../game/
 export async function initializeView() {
     // Gets Classic or Crazy Pong
     const gameType = await getGameType();
-    // Closes model, shows up remaining website
-    const gameTypeModal = document.getElementById("game-type-modal");
-    const gameSettingsMenu = document.getElementById("game-settings-menu");
-    if (gameTypeModal && gameSettingsMenu) {
-        gameTypeModal.classList.toggle("hidden");
-        gameSettingsMenu.classList.toggle("hidden");
-    }
-    // Toggles player-2-settings on
-    const player2section = document.getElementById("player-2-settings");
-    if (player2section)
-        player2section.classList.toggle("hidden");
     // Creates background loop
     createBackgroundLoop();
+    // Hides game type menu
+    const gameTypeSelection = document.getElementById("game-type-selection");
+    if (gameTypeSelection)
+        gameTypeSelection.classList.add("hidden");
+    else
+        console.warn("Game Type Selection not found.");
+    // Shows game settings menu
+    const gameSettingsMenu = document.getElementById("game-settings-menu");
+    if (gameSettingsMenu)
+        gameSettingsMenu.classList.remove("hidden");
+    else
+        console.warn("Game Settings Menu not found.");
+    // Toggle second player
+    const player2Settings = document.getElementById("player-2-settings");
+    if (player2Settings)
+        player2Settings.classList.remove("hidden");
+    else
+        console.warn("Player 2 Settings not found.");
     // If Crazy Pong, toggles character select section, adjusts sizes & activates character loop
     if (gameType === "crazy") {
-        const player1name = document.getElementById("player-1-name");
-        const player1paddle = document.getElementById("player-1-paddle-colour");
-        const player1char = document.getElementById("player-1-character");
-        if (player1name && player1paddle && player1char) {
-            player1name.classList.remove("h-[15%]");
-            player1name.classList.add("h-[50%]");
-            player1paddle.classList.remove("h-[15%]");
-            player1paddle.classList.add("h-[50%]");
-            player1char.classList.toggle("hidden");
-        }
-        const player2name = document.getElementById("player-2-name");
-        const player2paddle = document.getElementById("player-2-paddle-colour");
-        const player2char = document.getElementById("player-2-character");
-        if (player2name && player2paddle && player2char) {
-            player2name.classList.remove("h-[15%]");
-            player2name.classList.add("h-[50%]");
-            player2paddle.classList.remove("h-[15%]");
-            player2paddle.classList.add("h-[50%]");
-            player2char.classList.toggle("hidden");
-        }
-        // Creates character loop (for both players, if needed)
+        // Unhides character selection
+        const characterSelect1 = document.getElementById("player-1-character");
+        if (characterSelect1)
+            characterSelect1.classList.remove("hidden");
+        else
+            console.warn("Character Select 1 not found.");
+        const characterSelect2 = document.getElementById("player-2-character");
+        if (characterSelect2)
+            characterSelect2.classList.remove("hidden");
+        else
+            console.warn("Character Select 2 not found.");
+        // Creates character loop (for both players)
         createCharacterLoop();
         createCharacterLoop(2);
     }
