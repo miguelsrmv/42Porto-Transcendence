665ff7e25abff269b0f91bf189e27a3564b25108
Miguel Sá <miguel.srmv@gmail.com>
2025-04-18 17:21:51 +0100

    Added BackgroundData and CharacterData arrays for gameSetup screen

---
+++ srcs/requirements/frontend/src/features/game/backgroundData/backgroundData.types.ts
@@ -0,0 +1,18 @@
+/**
+ * @file backgroundData.types.ts
+ * @brief Defines the interface for background data in the game.
+ *
+ * This file contains the interface definition for background data, which includes
+ * properties for the background's name and the path to its image.
+ */
+
+/**
+ * @brief Interface for background data.
+ *
+ * This interface defines the structure for background data objects, including the background's name
+ * and the path to its image file.
+ */
+export interface backgroundData {
+  name: string;
+  imagePath: string;
+}
---
+++ srcs/requirements/frontend/src/features/game/characterData/characterData.ts
@@ -0,0 +1,93 @@
+/**
+ * @file characterData.ts
+ * @brief Provides character data and related functions for the game.
+ *
+ * This module defines the character data used in the game, including their names, attacks,
+ * and image paths. It also provides functions to retrieve character data.
+ */
+
+import { characterData } from './characterData.types.js';
+
+/**
+ * @brief Path to the character selection images.
+ */
+const characterSelectPicturePath = '../../../../static/character_select/';
+
+// TODO: Change characterAvatarPicture Path (I need to get proper avatars first)
+/**
+ * @brief Path to the character avatar images.
+ */
+const characterAvatarPicturePath = characterSelectPicturePath;
+
+/**
+ * @brief Array of character data objects.
+ *
+ * Each object contains the character's name, attack, image paths, and a help message.
+ */
+const characterList: characterData[] = [
+  {
+    name: 'Mario',
+    attack: 'Super Shroom',
+    characterSelectPicturePath: `${characterSelectPicturePath}mario.png`,
+    characterAvatarPicturePath: `${characterAvatarPicturePath}mario.png`,
+    selectHelpMessage: '',
+  },
+  {
+    name: 'Yoshi',
+    attack: 'Egg Barrage',
+    characterSelectPicturePath: `${characterSelectPicturePath}yoshi.png`,
+    characterAvatarPicturePath: `${characterAvatarPicturePath}yoshi.png`,
+    selectHelpMessage: '',
+  },
+  {
+    name: 'Sonic',
+    attack: 'Spin Dash',
+    characterSelectPicturePath: `${characterSelectPicturePath}sonic.png`,
+    characterAvatarPicturePath: `${characterAvatarPicturePath}sonic.png`,
+    selectHelpMessage: '',
+  },
+  {
+    name: 'Pikachu',
+    attack: 'Thunder Wave',
+    characterSelectPicturePath: `${characterSelectPicturePath}pikachu.png`,
+    characterAvatarPicturePath: `${characterAvatarPicturePath}pikachu.png`,
+    selectHelpMessage: '',
+  },
+  {
+    name: 'Mewtwo',
+    attack: 'Confusion',
+    characterSelectPicturePath: `${characterSelectPicturePath}mewtwo.png`,
+    characterAvatarPicturePath: `${characterAvatarPicturePath}mewtwo.png`,
+    selectHelpMessage: '',
+  },
+  {
+    name: 'Link',
+    attack: 'Hurricane Blade',
+    characterSelectPicturePath: `${characterSelectPicturePath}link.png`,
+    characterAvatarPicturePath: `${characterAvatarPicturePath}link.png`,
+    selectHelpMessage: '',
+  },
+  {
+    name: 'Samus',
+    attack: 'Missiles',
+    characterSelectPicturePath: `${characterSelectPicturePath}samus.png`,
+    characterAvatarPicturePath: `${characterAvatarPicturePath}samus.png`,
+    selectHelpMessage: '',
+  },
+  {
+    name: 'Donkey Kong',
+    attack: 'Giant Punch',
+    characterSelectPicturePath: `${characterSelectPicturePath}donkey_kong.png`,
+    characterAvatarPicturePath: `${characterAvatarPicturePath}donkey_kong.png`,
+    selectHelpMessage: '',
+  },
+] as const;
+
+/**
+ * @brief Retrieves all character data.
+ *
+ * @return The list of all character data.
+ */
+export function getAllCharacterData(): characterData[] {
+  return characterList;
+}
