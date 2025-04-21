76bddc11a2ff15077401ff2a9a797e71c58a5e11
damachad <davidmachado98@hotmail.com>
2025-04-21 15:38:52 +0100

    Add inputHandler to gameArea

--- srcs/requirements/frontend/src/features/game/gameApp/types.ts
+++ srcs/requirements/frontend/src/features/game/gameApp/types.ts
@@ -1,7 +1,10 @@
+import { InputHandler } from "./game";
+
 export interface GameArea {
   canvas: HTMLCanvasElement | null;
   context?: CanvasRenderingContext2D | null;
   interval?: number;
+  inputHandler: InputHandler | null;
   start(): void;
   clear(): void;
   stop(): void;
