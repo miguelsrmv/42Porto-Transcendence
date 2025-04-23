14d60b0c25962f00da80bb5590fc7b681cc571c9
Miguel Sá <miguel.srmv@gmail.com>
2025-04-23 16:41:02 +0100

    js

--- srcs/requirements/frontend/dist/features/game/gamePage.js
+++ srcs/requirements/frontend/dist/features/game/gamePage.js
@@ -1,5 +1,4 @@
-import { initializeGame } from './gameApp/game.js';
-import { getGameSettings } from './gameSetup.js';
+export {};
 /**
  * @file gamePage.ts
  * @brief Initializes the game page view.
@@ -12,53 +11,13 @@ import { getGameSettings } from './gameSetup.js';
  *
  * This function calls the `initializeGame` function to set up the game environment.
  * It is responsible for preparing the game page for user interaction.
- */
-export function initializeView() {
-    const gameSettings = getGameSettings();
-    if (gameSettings.gameType === 'Crazy Pong')
-        updateHUD(gameSettings.character1, gameSettings.character2);
-    // TODO: Add logic if Remote game
-    // TODO: Add logic if Tournament game
-    initializeGame(gameSettings);
-}
-function updateHUD(leftCharacter, rightCharacter) {
-    const leftCharHUD = document.getElementById('left-character-hud');
-    if (leftCharHUD)
-        leftCharHUD.classList.toggle('hidden');
-    const leftPortrait = document.getElementById('left-character-portrait');
-    if (leftPortrait)
-        leftPortrait.src = leftCharacter?.characterAvatarPicturePath;
-    const leftHUD = document.getElementById('left-hud');
-    if (leftHUD) {
-        const redElements = leftHUD?.querySelectorAll('.bg-red-500');
-        redElements.forEach((el) => {
-            el.classList.remove('bg-red-500');
-            el.classList.add(`bg-${leftCharacter?.accentColour}-500`);
-        });
-        const redBorderedElements = leftHUD?.querySelectorAll('.border-red-500');
-        redBorderedElements.forEach((el) => {
-            el.classList.remove('border-red-500');
-            el.classList.add(`border-${leftCharacter?.accentColour}-500`);
-        });
-    }
-    const rightCharHUD = document.getElementById('right-character-hud');
-    if (rightCharHUD)
-        rightCharHUD.classList.toggle('hidden');
-    const rightPortrait = document.getElementById('right-character-portrait');
-    if (rightPortrait)
-        rightPortrait.src = rightCharacter?.characterAvatarPicturePath;
-    const rightHUD = document.getElementById('right-hud');
-    if (rightHUD) {
-        const blueElements = rightHUD?.querySelectorAll('.bg-blue-500');
-        blueElements.forEach((el) => {
-            el.classList.remove('bg-blue-500');
-            el.classList.add(`bg-${rightCharacter?.accentColour}-500`);
-        });
-        const blueBordeblueElements = rightHUD?.querySelectorAll('.border-blue-500');
-        blueBordeblueElements.forEach((el) => {
-            el.classList.remove('border-blue-500');
-            el.classList.add(`border-${rightCharacter?.accentColour}-500`);
-        });
-    }
-}
+//  */
+// export function initializeView(): void {
+//   const gameSettings = getGameSettings();
+//   if (gameSettings.gameType === 'Crazy Pong')
+//     updateHUD(gameSettings.character1, gameSettings.character2);
+//   // TODO: Add logic if Remote game
+//   // TODO: Add logic if Tournament game
+//   initializeLocalGame(gameSettings);
+// }
 //# sourceMappingURL=gamePage.js.map
