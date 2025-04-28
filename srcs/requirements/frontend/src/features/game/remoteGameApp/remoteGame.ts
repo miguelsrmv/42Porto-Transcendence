6d76db72b9a32b671a55c42341f39da941473c52
Miguel Sá <miguel.srmv@gmail.com>
2025-04-28 11:08:54 +0100

    Now gets background from server

--- srcs/requirements/frontend/src/features/game/remoteGameApp/remoteGame.ts
+++ srcs/requirements/frontend/src/features/game/remoteGameApp/remoteGame.ts
@@ -1,19 +1,37 @@
 import type { gameSettings, leanGameSettings } from '../gameSettings/gameSettings.types.js';
+import { updateHUD } from '../gameSetup.js';
 
 export function initializeRemoteGame(leanGameSettings: leanGameSettings) {
   const webSocket = new WebSocket('wss://padaria.42.pt/ws');
 
-  const joinGameMsg = { type: 'join_game', playerSettings: leanGameSettings};
+  const joinGameMsg = { type: 'join_game', playerSettings: leanGameSettings };
 
   const serializedMsg = JSON.stringify(joinGameMsg);
 
-  webSocket.onopen = (event) => {
+  webSocket.onopen = () => {
     webSocket.send(serializedMsg);
   };
 
   webSocket.onmessage = (event) => {
-    console.log(event.data);
-  }
+    const messageData = JSON.parse(event.data);
+    if (messageData.type === 'game_setup') {
+      const gameSettings = messageData;
+      updateHUD(gameSettings, gameSettings.gameType);
+      updateBackground(gameSettings.background.imagePath);
+    }
+    // If a mensagem for game settings
+    //const gameSettings = JSON.parse(event.data);
+    // Else if a mensagem for dados de renderização
 
-  // TODO: Update HUD after, only then initialize the actual
+    // Else if a mensagem for game terminou e respetivos resultados
+  };
+}
+
+function updateBackground(backgroundPath: string): void {
+  const backgroundImg = document.getElementById('game-background') as HTMLImageElement;
+  if (!backgroundImg) {
+    console.warn("Couldn't find backgroundImg element");
+    return;
+  }
+  backgroundImg.src = backgroundPath;
 }
