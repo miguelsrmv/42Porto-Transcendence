7ea9e7a456af25ddc7f8442b465bc3fba25ff5f3
Miguel Sá <miguel.srmv@gmail.com>
2025-04-28 11:31:25 +0100

    Ping

--- srcs/requirements/frontend/src/features/game/remoteGameApp/remoteGame.ts
+++ srcs/requirements/frontend/src/features/game/remoteGameApp/remoteGame.ts
@@ -1,27 +1,40 @@
 import type { gameSettings, leanGameSettings } from '../gameSettings/gameSettings.types.js';
 import { updateHUD } from '../gameSetup.js';
+import { loadView } from '../../../core/viewLoader.js';
 
 export function initializeRemoteGame(leanGameSettings: leanGameSettings) {
   const webSocket = new WebSocket('wss://padaria.42.pt/ws');
 
   const joinGameMsg = { type: 'join_game', playerSettings: leanGameSettings };
 
-  const serializedMsg = JSON.stringify(joinGameMsg);
+  const serializedJoinGameMsg = JSON.stringify(joinGameMsg);
 
   webSocket.onopen = () => {
-    webSocket.send(serializedMsg);
+    // Sends leanGameSettings
+    webSocket.send(serializedJoinGameMsg);
+
+    // Starts sending ping-pong
+    setInterval(() => {
+      if (webSocket.readyState === WebSocket.OPEN) {
+        webSocket.send(JSON.stringify({ type: 'ping' }));
+      }
+    }, 30000);
   };
 
   webSocket.onmessage = (event) => {
     const messageData = JSON.parse(event.data);
+    // If message was the first type of message, load the view and update the HUD and background
     if (messageData.type === 'game_setup') {
-      const gameSettings = messageData;
+      const gameSettings = messageData.settings;
+      // TODO: Add in a modal before?
+      loadView('game-page');
       updateHUD(gameSettings, gameSettings.gameType);
       updateBackground(gameSettings.background.imagePath);
     }
-    // If a mensagem for game settings
-    //const gameSettings = JSON.parse(event.data);
     // Else if a mensagem for dados de renderização
+    //{
+    //renderGame(serverData);
+    //}
 
     // Else if a mensagem for game terminou e respetivos resultados
   };
