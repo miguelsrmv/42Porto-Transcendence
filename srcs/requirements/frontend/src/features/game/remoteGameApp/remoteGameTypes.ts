9ec52356a6c3674a5607f085d673b523eb4b2833
Miguel Sa <miguel.srmv@gmail.com>
2025-04-30 14:15:26 +0000

    Remote is working I Think

--- srcs/requirements/frontend/src/features/game/remoteGameApp/remoteGameTypes.ts
+++ srcs/requirements/frontend/src/features/game/remoteGameApp/remoteGameTypes.ts
@@ -1,12 +1,30 @@
-import { Ball } from '../localGameApp/ball.js';
-import { Paddle } from '../localGameApp/paddle.js';
-
 export interface GameArea {
   canvas: HTMLCanvasElement | null;
   context?: CanvasRenderingContext2D | null;
   start(): void;
   clear(): void;
   stop(): void;
+}
+
+export interface Ball {
+  x: number;
+  y: number;
+  previousX: number;
+  previousY: number;
+  radius: number;
+  speedX: number;
+  speedY: number;
+  isVisible: boolean;
+}
+
+export interface Paddle {
+  width: number;
+  height: number;
+  color: string;
+  x: number;
+  y: number;
+  speedY: number;
+  speedModifier: number;
 }
 
 export interface GameState {
