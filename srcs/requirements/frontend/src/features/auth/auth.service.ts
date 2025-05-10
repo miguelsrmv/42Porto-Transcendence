ebfaad2a404a875b18e923e1099c1603f5f78e11
Miguel Sá <miguel.srmv@gmail.com>
2025-05-10 17:25:03 +0100

    Clean Login / Register form on modal toggle

--- srcs/requirements/frontend/src/features/auth/auth.service.ts
+++ srcs/requirements/frontend/src/features/auth/auth.service.ts
@@ -17,10 +17,10 @@ import { loginErrorMessages, registerErrorMessages } from '../../constants/error
  *
  * @param event The form submission event.
  */
-export async function attemptLogin(this: HTMLFormElement, event: Event) {
+export async function attemptLogin(form: HTMLFormElement, event: Event) {
   event.preventDefault(); // Prevent default form submission
 
-  const data = formToJSON(this);
+  const data = formToJSON(form);
 
   try {
     const response = await fetch('/api/users/login', {
