82825d3069584e7f18178b57554bb5c14a57caa0
tiaferna <tiaferna@student.42porto.com>
2025-05-14 13:01:18 +0100

    updating to match blockchain logic

--- srcs/requirements/backend/src/api/services/tournament.services.ts
+++ srcs/requirements/backend/src/api/services/tournament.services.ts
@@ -2,22 +2,22 @@ import { TournamentPlayer } from '../controllers/tournament.controller';
 import { prisma } from '../../utils/prisma';
 
 export async function createTournamentParticipant(participant: TournamentPlayer) {
-  const tournament = await prisma.tournament.findUniqueOrThrow({
-    where: { id: participant.tournamentId },
-    include: { participants: true },
-  });
-  if (tournament.participants.length >= tournament.maxParticipants)
-    throw new Error('Tournament is full');
+  // const tournament = await prisma.tournament.findUniqueOrThrow({
+  //   where: { id: participant.tournamentId },
+  //   include: { participants: true },
+  // });
+  // if (tournament.participants.length >= tournament.maxParticipants)
+  //   throw new Error('Tournament is full');
 
-  const registeredPlayer = await prisma.tournamentParticipant.findUnique({
-    where: {
-      tournamentId_userId: {
-        tournamentId: participant.tournamentId!,
-        userId: participant.userId,
-      },
-    },
-  });
-  if (registeredPlayer) throw new Error('Player is already registered in this tournament');
+  // const registeredPlayer = await prisma.tournamentParticipant.findUnique({
+  //   where: {
+  //     tournamentId_userId: {
+  //       // tournamentId: participant.tournamentId!,
+  //       userId: participant.userId,
+  //     },
+  //   },
+  // });
+  // if (registeredPlayer) throw new Error('Player is already registered in this tournament');
 
   return await prisma.tournamentParticipant.create({
     data: {
