f16f2bdcd8d71cd65e2ef7bcae303f0ba1e2d2aa
damachad <davidmachado98@hotmail.com>
2025-05-19 16:32:20 +0100

    Change GameSession to a class

--- srcs/requirements/backend/src/ws/remoteGameApp/sessionManagement.ts
+++ srcs/requirements/backend/src/ws/remoteGameApp/sessionManagement.ts
@@ -1,52 +1,24 @@
 import WebSocket from 'ws';
-import { background, gameSettings, leanGameSettings } from './settings';
+import { background, leanGameSettings } from './settings';
 import { getBackgroundList } from './backgroundData';
-import { GameSession, GameSessionSerializable } from './types';
+import { GameSessionSerializable } from './types';
 import { prisma } from '../../utils/prisma';
 import app from '../../app';
+import { GameSession } from '../gameSession';
 
 const classicPongSessions: GameSession[] = [];
 const crazyPongSessions: GameSession[] = [];
 
-const DEFAULT_AVATAR_PATH = '../../../../static/avatar/default/mario.png';
-function getRandomBackground(): background {
+export const DEFAULT_AVATAR_PATH = '../../../../static/avatar/default/mario.png';
+export function getRandomBackground(): background {
   const backgroundList = getBackgroundList();
   return backgroundList[Math.floor(Math.random() * backgroundList.length)];
 }
 
-export async function mergePlayer2IntoGameSettings(
-  matchSettings: gameSettings,
-  playerSettings: leanGameSettings,
-) {
-  return {
-    ...matchSettings,
-    alias2: playerSettings.alias,
-    avatar2: (await getAvatarFromPlayer(playerSettings.playerID)) || DEFAULT_AVATAR_PATH,
-    paddleColour2: playerSettings.paddleColour,
-    character2: playerSettings.character,
-  };
-}
-
-async function getAvatarFromPlayer(playerID: string) {
+export async function getAvatarFromPlayer(playerID: string) {
   const user = await prisma.user.findUniqueOrThrow({ where: { id: playerID } });
   if (!user) app.log.error('User not found in getAvatarFromPlayer');
   return user.avatarUrl;
-}
-
-export async function mergePlayer1IntoGameSettings(playerSettings: leanGameSettings) {
-  return {
-    playType: playerSettings.playType,
-    gameType: playerSettings.gameType,
-    alias1: playerSettings.alias,
-    alias2: 'player2',
-    avatar1: (await getAvatarFromPlayer(playerSettings.playerID)) || DEFAULT_AVATAR_PATH,
-    avatar2: DEFAULT_AVATAR_PATH,
-    paddleColour1: playerSettings.paddleColour,
-    paddleColour2: '#ff0000',
-    character1: playerSettings.character,
-    character2: playerSettings.character,
-    background: getRandomBackground(),
-  };
 }
 
 function gameTypeToSessions(type: string) {
@@ -58,7 +30,7 @@ async function foundSession(ws: WebSocket, playerSettings: leanGameSettings) {
   for (const session of gameTypeToSessions(playerSettings.gameType)) {
     if (session.players.size === 1) {
       session.players.set(ws, playerSettings.playerID);
-      session.settings = await mergePlayer2IntoGameSettings(session.settings, playerSettings);
+      await session.mergePlayer2IntoGameSettings(playerSettings);
 
       // For testing purposes
       const serializableSession: GameSessionSerializable = {
@@ -77,11 +49,8 @@ async function foundSession(ws: WebSocket, playerSettings: leanGameSettings) {
 }
 
 async function createSession(ws: WebSocket, playerSettings: leanGameSettings) {
-  const newSession: GameSession = {
-    players: new Map<WebSocket, string>([[ws, playerSettings.playerID]]),
-    settings: await mergePlayer1IntoGameSettings(playerSettings),
-    gameArea: null,
-  };
+  const newSession = new GameSession(ws, playerSettings);
+  await newSession.updateAvatar1(playerSettings.playerID);
 
   // For testing purposes
   const serializableSession: GameSessionSerializable = {
@@ -118,11 +87,6 @@ export function getGameSession(playerSocket: WebSocket): GameSession | null {
     }
   }
   return null;
-}
-
-export function isSessionFull(session: GameSession): boolean {
-  if (session.players.size === 2) return true;
-  return false;
 }
 
 export function playerIsInASession(playerId: string): boolean {
