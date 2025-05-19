64e9bd3c3cbda8437f233aa14ca59cc6b26b3ace
tiaferna <tiaferna@student.42porto.com>
2025-05-19 16:59:57 +0100

    fetches contract address from volume

--- srcs/requirements/backend/src/api/services/blockchain.services.ts
+++ srcs/requirements/backend/src/api/services/blockchain.services.ts
@@ -1,10 +1,13 @@
 import { ethers } from 'ethers';
 import { abi } from './TournamentsStorageABI'
+import dotenv from "dotenv"
+import fs from 'fs';
+
+dotenv.config();
 
 const RPC_URL = 'https://api.avax-test.network/ext/bc/C/rpc'; // C-Chain
+const CONTRACT_ADDRESS = fs.readFileSync('/app/data/blockchain/blockchain_address.txt', 'utf-8').trim();
 const PRIVATE_KEY = process.env.PRIVATE_KEY!;
-const CONTRACT_ADDRESS = '0x013f7a4A471450D51678a27568Dc224f208462A9';
-
 const provider = new ethers.JsonRpcProvider(RPC_URL);
 const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, wallet);
