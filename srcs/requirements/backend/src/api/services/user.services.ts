5a98f6eabfee84c28e81d96fb7d871ea00bdb9ef
damachad <davidmachado98@hotmail.com>
2025-05-21 09:22:15 +0100

    Change stats route to match current points logic

--- srcs/requirements/backend/src/api/services/user.services.ts
+++ srcs/requirements/backend/src/api/services/user.services.ts
@@ -1,6 +1,7 @@
 import { Match, GameMode } from '@prisma/client';
 import { prisma } from '../../utils/prisma';
 
+// Unused
 export function getUserClassicStats(matches: Match[], userId: string) {
   const classicMatches = matches.filter((match) => match.mode === GameMode.CLASSIC);
   const totalMatches = classicMatches.length;
@@ -16,6 +17,7 @@ export function getUserClassicStats(matches: Match[], userId: string) {
   };
 }
 
+// Unused
 export function getUserCrazyStats(matches: Match[], userId: string) {
   const crazyMatches = matches.filter((match) => match.mode === GameMode.CRAZY);
   const totalMatches = crazyMatches.length;
@@ -28,6 +30,21 @@ export function getUserCrazyStats(matches: Match[], userId: string) {
     losses,
     winRate: parseFloat(((wins / totalMatches) * 100).toFixed(2)) || 0,
     points: wins * 3 + losses, // Assuming 3 points for a win and 1 for a loss
+  };
+}
+
+export async function getUserGlobalStats(matches: Match[], userId: string) {
+  const totalMatches = matches.length;
+  const wins = matches.filter((match) => match.winnerId === userId).length;
+  const losses = totalMatches - wins;
+
+  return {
+    totalMatches,
+    wins,
+    losses,
+    winRate: parseFloat(((wins / totalMatches) * 100).toFixed(2)) || 0,
+    points: wins, // Assuming 1 point for a win and 0 for a loss
+    rank: await getUserRank(userId),
   };
 }
 
