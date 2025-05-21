496b449fa390b4e56074375c6ee93d1a5edee057
Miguel Sa <miguel.srmv@gmail.com>
2025-05-21 11:30:30 +0100

    Starting the leaderboard

---
+++ srcs/requirements/frontend/src/features/rankings/rankings.types.ts
@@ -0,0 +1,14 @@
+export interface matchData {
+  createdAt: string;
+  id: string;
+  mode: string;
+  settings: string;
+  updatedAt: string;
+  user1Character: string;
+  user1Id: string;
+  user1Score: number;
+  user2Character: string;
+  user2Id: string;
+  user2Score: number;
+  winnerId: string;
+}
