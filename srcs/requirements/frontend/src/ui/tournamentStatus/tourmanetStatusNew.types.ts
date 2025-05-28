f65d8e2a39337ff0d684d340c98dc3eba9123fa5
Miguel Sá <miguel.srmv@gmail.com>
2025-05-28 19:10:21 +0100

    New logic for Tournament Tree

--- srcs/requirements/frontend/src/ui/tournamentStatus/tourmanetStatusNew.types.ts
+++
@@ -1,7 +0,0 @@
-export interface tournamentPlayer {
-  userAlias: string; // Tournament Alias
-  quarterFinalScore: string | null; // Null before first game starts
-  semiFinalScore: string | null; // Null if lost at quarterFinals
-  finalScore: string | null; // Null if lost at semifinals
-  avatarPath: string;
-}
