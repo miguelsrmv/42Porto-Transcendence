ba96bfd226ce58cf781a50b3ca63c9ec1d37fcc0
damachad <davidmachado98@hotmail.com>
2025-05-30 17:16:58 +0100

    Get avatarPath from DB

--- srcs/requirements/backend/src/api/services/tournament.services.ts
+++ srcs/requirements/backend/src/api/services/tournament.services.ts
@@ -7,6 +7,7 @@ export async function generateTournamentData(tournamentId: string) {
     take: 8,
     select: {
       id: true,
+      avatarUrl: true,
     },
   });
   if (users.length < 8) throw 'Less than 8 users in the database';
@@ -14,7 +15,7 @@ export async function generateTournamentData(tournamentId: string) {
     {
       id: users[0].id,
       userAlias: 'alias1',
-      avatarPath: '../../../../static/avatar/default/mario.png',
+      avatarPath: users[0].avatarUrl,
       quarterFinalScore: '5',
       semiFinalScore: '3',
       finalScore: '',
@@ -22,7 +23,7 @@ export async function generateTournamentData(tournamentId: string) {
     {
       id: users[1].id,
       userAlias: 'alias2',
-      avatarPath: '../../../../static/avatar/default/bowser.png',
+      avatarPath: users[1].avatarUrl,
       quarterFinalScore: '5',
       semiFinalScore: '5',
       finalScore: '2',
@@ -30,7 +31,7 @@ export async function generateTournamentData(tournamentId: string) {
     {
       id: users[2].id,
       userAlias: 'alias3',
-      avatarPath: '../../../../static/avatar/default/cloud.png',
+      avatarPath: users[2].avatarUrl,
       quarterFinalScore: '5',
       semiFinalScore: '5',
       finalScore: '5',
@@ -38,7 +39,7 @@ export async function generateTournamentData(tournamentId: string) {
     {
       id: users[3].id,
       userAlias: 'alias4',
-      avatarPath: '../../../../static/avatar/default/chrom.png',
+      avatarPath: users[3].avatarUrl,
       quarterFinalScore: '3',
       semiFinalScore: '',
       finalScore: '',
@@ -46,7 +47,7 @@ export async function generateTournamentData(tournamentId: string) {
     {
       id: users[4].id,
       userAlias: 'alias5',
-      avatarPath: '../../../../static/avatar/default/corrin.png',
+      avatarPath: users[4].avatarUrl,
       quarterFinalScore: '2',
       semiFinalScore: '',
       finalScore: '',
@@ -54,7 +55,7 @@ export async function generateTournamentData(tournamentId: string) {
     {
       id: users[5].id,
       userAlias: 'alias6',
-      avatarPath: '../../../../static/avatar/default/daisy.png',
+      avatarPath: users[5].avatarUrl,
       quarterFinalScore: '5',
       semiFinalScore: '3',
       finalScore: '',
@@ -62,7 +63,7 @@ export async function generateTournamentData(tournamentId: string) {
     {
       id: users[6].id,
       userAlias: 'alias7',
-      avatarPath: '../../../../static/avatar/default/bowser_jr.png',
+      avatarPath: users[6].avatarUrl,
       quarterFinalScore: '4',
       semiFinalScore: '',
       finalScore: '',
@@ -70,7 +71,7 @@ export async function generateTournamentData(tournamentId: string) {
     {
       id: users[7].id,
       userAlias: 'alias8',
-      avatarPath: '../../../../static/avatar/default/bayonetta.png',
+      avatarPath: users[7].avatarUrl,
       quarterFinalScore: '1',
       semiFinalScore: '',
       finalScore: '',
