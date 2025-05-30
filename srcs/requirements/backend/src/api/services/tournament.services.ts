276f1968213a7080ce701476751604902c5c3ce3
damachad <davidmachado98@hotmail.com>
2025-05-30 16:42:03 +0100

    Add mock data for tournament/:id route

--- srcs/requirements/backend/src/api/services/tournament.services.ts
+++ srcs/requirements/backend/src/api/services/tournament.services.ts
@@ -1,6 +1,78 @@
 // import { TournamentPlayer } from '../controllers/tournament.controller';
 // import { prisma } from '../../utils/prisma';
 
+import { tournamentPlayer } from '../../ws/remoteGameApp/types';
+
+export function generateTournamentData(tournamentId: string) {
+  const tournamentData: tournamentPlayer[] = [
+    {
+      id: tournamentId,
+      userAlias: 'alias1',
+      avatarPath: '../../../../static/avatar/default/mario.png',
+      quarterFinalScore: '5',
+      semiFinalScore: '3',
+      finalScore: '',
+    },
+    {
+      id: tournamentId,
+      userAlias: 'alias2',
+      avatarPath: '../../../../static/avatar/default/bowser.png',
+      quarterFinalScore: '5',
+      semiFinalScore: '5',
+      finalScore: '2',
+    },
+    {
+      id: tournamentId,
+      userAlias: 'alias3',
+      avatarPath: '../../../../static/avatar/default/cloud.png',
+      quarterFinalScore: '5',
+      semiFinalScore: '5',
+      finalScore: '5',
+    },
+    {
+      id: tournamentId,
+      userAlias: 'alias4',
+      avatarPath: '../../../../static/avatar/default/chrom.png',
+      quarterFinalScore: '3',
+      semiFinalScore: '',
+      finalScore: '',
+    },
+    {
+      id: tournamentId,
+      userAlias: 'alias5',
+      avatarPath: '../../../../static/avatar/default/corrin.png',
+      quarterFinalScore: '2',
+      semiFinalScore: '',
+      finalScore: '',
+    },
+    {
+      id: tournamentId,
+      userAlias: 'alias6',
+      avatarPath: '../../../../static/avatar/default/daisy.png',
+      quarterFinalScore: '5',
+      semiFinalScore: '3',
+      finalScore: '',
+    },
+    {
+      id: tournamentId,
+      userAlias: 'alias7',
+      avatarPath: '../../../../static/avatar/default/bowser_jr.png',
+      quarterFinalScore: '4',
+      semiFinalScore: '',
+      finalScore: '',
+    },
+    {
+      id: tournamentId,
+      userAlias: 'alias8',
+      avatarPath: '../../../../static/avatar/default/bayonetta.png',
+      quarterFinalScore: '1',
+      semiFinalScore: '',
+      finalScore: '',
+    },
+  ];
+  return tournamentData;
+}
+
 // // export async function createTournamentParticipant(participant: TournamentPlayer) {
 // //   const tournament = await prisma.tournament.findUniqueOrThrow({
 // //     where: { id: participant.tournamentId },
