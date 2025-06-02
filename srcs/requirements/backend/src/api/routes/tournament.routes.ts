9550ddaa164b5c7a4ef21fef85840ba3eb796acd
damachad <davidmachado98@hotmail.com>
2025-06-02 17:41:22 +0100

    Refactor getUserLastTournaments name

--- srcs/requirements/backend/src/api/routes/tournament.routes.ts
+++ srcs/requirements/backend/src/api/routes/tournament.routes.ts
@@ -1,12 +1,9 @@
 import { FastifyInstance } from 'fastify';
 import { getByIdSchema } from '../schemas/global.schema';
-import {
-  getTournamentById,
-  getUserLastThreeTournaments,
-} from '../controllers/tournament.controller';
+import { getTournamentById, getUserLastTournaments } from '../controllers/tournament.controller';
 
 // NOTE: Insert '{ onRequest: [fastify.jwtAuth] }' before handler to protect route
 export async function tournamentRoutes(fastify: FastifyInstance) {
-  fastify.get('/user/:id', { schema: getByIdSchema }, getUserLastThreeTournaments);
+  fastify.get('/user/:id', { schema: getByIdSchema }, getUserLastTournaments);
   fastify.get('/:id', { schema: getByIdSchema }, getTournamentById);
 }
