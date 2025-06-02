d427cb79fb21150f378b155847b95cf80e295795
damachad <davidmachado98@hotmail.com>
2025-06-02 17:38:54 +0100

    Remove commented lines

--- srcs/requirements/backend/src/api/routes/tournament.routes.ts
+++ srcs/requirements/backend/src/api/routes/tournament.routes.ts
@@ -7,12 +7,6 @@ import {
 
 // NOTE: Insert '{ onRequest: [fastify.jwtAuth] }' before handler to protect route
 export async function tournamentRoutes(fastify: FastifyInstance) {
-  // fastify.get('/', getAllTournaments);
   fastify.get('/user/:id', { schema: getByIdSchema }, getUserLastThreeTournaments);
   fastify.get('/:id', { schema: getByIdSchema }, getTournamentById);
-  // fastify.post('/', { schema: createTournamentSchema }, createTournament);
-  // fastify.post('/newTournament', tournamentBlockchain);
-  // fastify.patch('/:id', { schema: updateTournamentSchema }, updateTournament);
-  // fastify.delete('/:id', { schema: getByIdSchema }, deleteTournament);
-  // fastify.post('/participant', { schema: tournamentParticipantSchema }, addPlayerToTournament);
 }
