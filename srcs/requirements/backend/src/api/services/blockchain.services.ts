fb714042d932bba3d35fb0f5d37b62b2fa942a45
Tiago Martins <tiago86martins@gmail.com>
2025-06-03 16:32:13 +0100

    updated contract

--- srcs/requirements/backend/src/api/services/blockchain.services.ts
+++ srcs/requirements/backend/src/api/services/blockchain.services.ts
@@ -6,7 +6,7 @@ import fs from 'fs';
 dotenv.config();
 
 const RPC_URL = 'https://api.avax-test.network/ext/bc/C/rpc'; // C-Chain
-const CONTRACT_ADDRESS = "0x10D635aE0e9424bd196345e6D5AB9Aa0616a9F40";/* fs
+const CONTRACT_ADDRESS = "0xb881D6F7CD48F6dE99E888600c46A1407b0D3913";/* fs
   .readFileSync('/app/data/blockchain/blockchain_address.txt', 'utf-8')
   .trim(); */
 const PRIVATE_KEY = process.env.PRIVATE_KEY!;
