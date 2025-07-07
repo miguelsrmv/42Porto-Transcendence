3fdf55bdb8a5e13d74f248bed5a067c32a59bd76
damachad <davidmachado98@hotmail.com>
2025-07-07 15:26:05 +0100

    Add return on error

--- srcs/requirements/backend/src/api/validation/users.validation.ts
+++ srcs/requirements/backend/src/api/validation/users.validation.ts
@@ -41,14 +41,13 @@ export async function userUpdateValidation(
   request.body = removeEmptyStrings(request.body);
   for (const [key, value] of Object.entries(request.body)) {
     if (typeof value === 'string' && value.trim() === '')
-      reply.code(400).send({ message: `${key} cannot be empty or whitespace` });
+      return reply.code(400).send({ message: `${key} cannot be empty or whitespace` });
     else if (value.length > 320)
-      reply.code(400).send({ message: `${key} cannot have over 320 characters` });
+      return reply.code(400).send({ message: `${key} cannot have over 320 characters` });
     else if (key !== 'email' && hasInvalidChars(value))
-      reply.code(400).send({ message: `${key} cannot have invalid characters` });
+      return reply.code(400).send({ message: `${key} cannot have invalid characters` });
     else if (key === 'email' && !isValidEmail(value))
-      reply.code(400).send({ message: `Invalid email format` });
-    return;
+      return reply.code(400).send({ message: `Invalid email format` });
   }
 
   const user = await prisma.user.findUniqueOrThrow({ where: { id: request.user.id } });
