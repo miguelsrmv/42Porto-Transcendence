65c8667de5503725ea097e5b26f8ff5efbb92a09
damachad <davidmachado98@hotmail.com>
2025-07-09 09:49:52 +0100

    Add error messages for input too long

--- srcs/requirements/frontend/src/constants/errorMessages.ts
+++ srcs/requirements/frontend/src/constants/errorMessages.ts
@@ -7,6 +7,8 @@ type ErrorMessages = {
 export const loginErrorMessages: ErrorMessages = {
   'body/password must NOT have fewer than 6 characters': 'Invalid Password',
   'body/email must match format "email"': 'Invalid Email',
+  'body/password must NOT have more than 72 characters': 'Password too long (> 72 characters)',
+  'body/email must NOT have more than 254 characters': 'Email too long (> 254 characters)',
   'Invalid credentials': 'Email and Password do not match',
   'An operation failed because it depends on one or more records that were required but not found. No record was found for a query.':
     'User not found',
@@ -17,13 +19,14 @@ export const loginErrorMessages: ErrorMessages = {
 export const registerErrorMessages: ErrorMessages = {
   'body/username must NOT have fewer than 3 characters': 'Invalid Username (< 3 characters)',
   'body/email must match format "email"': 'Invalid Email',
-  'body/password must NOT have fewer than 6 characters': 'Invalid Password',
+  'body/password must NOT have fewer than 6 characters': 'Invalid Password (< 6 characters)',
+  'body/password must NOT have more than 72 characters': 'Password too long (> 72 characters)',
+  'body/email must NOT have more than 254 characters': 'Email too long (> 254 characters)',
+  'body/username must NOT have more than 32 characters': 'Username too long (> 32 characters)',
   'Passwords do not match': 'Passwords do not match',
   'Unique constraint failed on the fields: (`username`)': 'Username already in use',
   'Unique constraint failed on the fields: (`email`)': 'Email already in use',
   'username cannot have invalid characters': 'Invalid Username',
-  'username cannot be empty or whitespace': 'Username field is mandatory',
-  'email cannot be empty or whitespace': 'Email field is mandatory',
   'Invalid email format': 'Invalid Email',
   'Password must include an uppercase letter, a lowercase letter, and a number':
     'Password must include an uppercase letter, a lowercase letter, and a number',
