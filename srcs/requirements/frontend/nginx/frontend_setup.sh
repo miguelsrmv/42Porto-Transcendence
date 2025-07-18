e7b68dd795ba48c82c9e9197a84d0635fd33a34f
damachad <davidmachado98@hotmail.com>
2025-07-18 14:42:58 +0100

    Remove UID as USER is not defined

--- srcs/requirements/frontend/nginx/frontend_setup.sh
+++ srcs/requirements/frontend/nginx/frontend_setup.sh
@@ -11,7 +11,7 @@ if [ ! -f /etc/ssl/nginx-selfsigned.pem ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -out /etc/ssl/nginx-selfsigned.pem \
         -keyout /etc/ssl/nginx-selfsigned.key \
-        -subj "/C=PT/ST=Porto/L=Porto/O=42/OU=42/CN=${DOMAIN_NAME}/UID=${USER}"
+        -subj "/C=PT/ST=Porto/L=Porto/O=42/OU=42/CN=${DOMAIN_NAME}"
 else
     echo "SSL certificate already exists, skipping generation."
 fi
