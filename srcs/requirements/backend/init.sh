5948959787a3a2f8f77091eae54bf1874e364809
damachad <davidmachado98@hotmail.com>
2025-07-23 10:33:38 +0100

    Change entrypoint

--- srcs/requirements/backend/init.sh
+++ srcs/requirements/backend/init.sh
@@ -4,4 +4,4 @@ npx prisma migrate deploy                       # Apply migrations to the databa
 npx prisma generate								# Generate prisma client
 npm run build                                   # Transpile the .ts files into .js files in dist/
 chmod -R 755 ./dist                             # Give execution permission to dist/
-exec npm run start                              # Execute dist/server.js
+exec node ./dist/src/server.js                    # Execute dist/server.js
